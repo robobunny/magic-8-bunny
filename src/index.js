794af2589172554db7d8a4a4cf8274f14330daa2
William Duraney <william.duraney@gmail.com>
2021-01-19 12:19:44 -0500

    update dependencies

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from './components/App';
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./components/App"
+import "./styles/styles.scss"
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"))
