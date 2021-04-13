4e35a4499cc306dfb22d271d840fb95986767e5a
William Duraney <william.duraney@gmail.com>
2021-04-13 17:24:08 -0400

    add unit and integration tests

--- src/__test__/components/DisplayAnswer.test.js
+++ src/__test__/components/DisplayAnswer.test.js
@@ -1,56 +1,38 @@
 import React from "react"
 import DisplayAnswer from "../../components/DisplayAnswer/DisplayAnswer"
-import { getAllByText, render } from "@testing-library/react"
+import { cleanup, getAllByText, render } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 
 const mockCloseModal = jest.fn()
-mockCloseModal.mockImplementation(() => "foobar")
 
 describe("DisplayAnswer component", () => {
-  //   describe("when there is an answer selected", () => {
-  //     const { getByAltText, getByText, unmount } = render(
-  //       <DisplayAnswer selectedOption={"Test answer 1"} closeModal={mockCloseModal} />
-  //     )
-  //     test("should display image", () => {
-  //       const roboBunny = getByAltText("A cute green robobunny character with square glasses")
-  //       expect(roboBunny).toBeInTheDocument()
-  //     })
+  test("should display the selected answer", () => {
+    const { getByText, getByAltText } = render(<DisplayAnswer selectedOption="Test response 1" />)
+    const displayedAnswer = getByText(/Test response 1/)
+    expect(displayedAnswer).toBeVisible()
+    const acceptButton = getByText("I accept your wisdom!")
+    expect(acceptButton).toBeVisible()
+    const bunnyPic = getByAltText("A cute green robobunny character with square glasses")
+    expect(bunnyPic).toBeVisible()
+    cleanup()
+  })
 
-  //     test("should display text", () => {
-  //       const answerText = getByText(/test answer/i)
-  //       expect(answerText).toBeInTheDocument()
-  //     })
+  test("should display modal only when selected answer is given", () => {
+    const { queryByText, queryByAltText } = render(<DisplayAnswer selectedOption={undefined} />)
+    const bunnyPic = queryByAltText("A cute green robobunny character with square glasses")
+    expect(bunnyPic).not.toBeInTheDocument()
+    const heading = queryByText(/Robobunny has spoken/)
+    expect(heading).not.toBeInTheDocument()
+    cleanup()
+  })
 
-  //     test("should display button", () => {
-  //       const acceptButton = getByText("I accept your wisdom!")
-  //       expect(acceptButton).toBeInTheDocument()
-  //     })
-
-  //     test("should close modal on button click", () => {
-  //       const acceptButton = getByText("I accept your wisdom!")
-  //       userEvent.click(acceptButton)
-  //       expect(mockCloseModal).toHaveBeenCalled()
-  //     })
-
-  //     unmount()
-  //   })
-
-  //   describe("when there is no option selected", () => {
-  //     const { queryByAltText, queryByText, unmount } = render(
-  //       <DisplayAnswer selectedOption={undefined} closeModal={mockCloseModal} />
-  //     )
-  //     const roboBunny = queryByAltText("A cute green robobunny character with square glasses")
-  //     const answerText = queryByText(/test answer/i)
-  //     const acceptButton = queryByText("I accept your wisdom!")
-
-  //     test("should not display anything", () => {
-  //       expect(roboBunny).not.toBeInTheDocument()
-  //       expect(answerText).not.toBeInTheDocument()
-  //       expect(acceptButton).not.toBeInTheDocument()
-  //     })
-  //     unmount()
-  //   })
-  test("expect there to be tests", () => {
-    expect(false).toBe(true)
+  test("should call closeModal method when button is pressed", () => {
+    const { getByText } = render(
+      <DisplayAnswer selectedOption="Test response 1" closeModal={mockCloseModal} />
+    )
+    const acceptButton = getByText("I accept your wisdom!")
+    userEvent.click(acceptButton)
+    expect(mockCloseModal).toHaveBeenCalled()
+    cleanup()
   })
 })
